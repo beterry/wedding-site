d0fc5c4ab746738bb5c820e52789080f0ba86659
Ben Terry <beterry12@gmail.com>
2021-11-04 16:08:32 -0400

    copy revisions

--- wedding-site.js
+++ wedding-site.js
@@ -1,46 +1,14 @@
-let numberOfNames = 2;
-
-const addNameButton = document.getElementById('addName');
-const nameGroup = document.getElementById('nameGroup');
-
-addNameButton.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    numberOfNames += 1;
-
-    if (numberOfNames <= 6){
-        nameGroup.insertAdjacentHTML('beforeend', `
-            <div class="form-group">
-                <input type="text" id="name-${numberOfNames}" name="Name ${numberOfNames}" placeholder="Add another name" required/>
-                <div class="radio-group">
-                    <div class="attending-group">
-                        <input type="radio" id="attending-${numberOfNames}" name="Name ${numberOfNames} Status" value="Will attend" checked>
-                        <label for="attending-${numberOfNames}">Attending</label>
-                    </div>
-                    <div class="attending-group">
-                        <input type="radio" id="not-attending-${numberOfNames}" name="Name ${numberOfNames} Status" value="Will not attend">
-                        <label for="not-attending-${numberOfNames}">Not Attending</label>
-                    </div>
-                </div>
-            </div>
-        `);
-    } else {
-        addNameButton.style.display = 'none';
-    }
-
-})
-
-var form = document.getElementById("rsvp-form");
-var formMessage = document.getElementById("form-message");
+const form = document.getElementById("rsvp-form");
+const formMessage = document.getElementById("form-message");
 form.onsubmit = function (event) {
     event.preventDefault();
-    var formData = new FormData(form);
-    var xhr = new XMLHttpRequest();
+    let formData = new FormData(form);
+    let xhr = new XMLHttpRequest();
     xhr.open("POST", form.action, true);
     xhr.onload = function (e) {
         console.log(xhr);
         if (xhr.status === 200) {
-            formMessage.innerHTML = "Thank you! You RSVP has been sent to Ben & Sophie.";
+            formMessage.innerHTML = "Thank you! Your RSVP has been sent to Ben & Sophie.";
         } else {
             formMessage.innerHTML = "Oops! Something went wrong when submitting the form. Ben probably messed up when he was coding this. Would you mind texting Ben to explain he messed up the form: 717-419-0478.";
         }
